aba25376647f6340c2ef85eff47946573e61590e
aram <leandrojavr@gmail.com>
2020-06-13 06:27:23 -0300

    Read status file, add get_line function

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ release: main
 fuzz: main_fuzz
 
 main:
-	clang src/main.c src/lyrics.c -lncurses -o main -std=c99 -ggdb -Isrc/
+	clang src/main.c src/lyrics.c -lncurses -o build/main -std=c99 -ggdb -Isrc/
 
 main_fuzz:
-	afl-gcc tests/test.c src/lyrics.c -lncurses -o main_fuzzable -std=c11 -ggdb -D_GNU_SOURCE -Isrc/
+	afl-gcc tests/test.c src/lyrics.c -lncurses -o build/main_fuzzable -std=c11 -ggdb -D_GNU_SOURCE -Isrc/
--- src/main.c
+++ src/main.c
@@ -1,16 +1,68 @@
 #include <lyrics.h>
+#include <stdlib.h>
+#include <fcntl.h>
 #include <sys/ioctl.h>
 #include <ncurses.h>
 
+
 const char *status_path = "/home/maek/cmus-status.txt";
+
+
+int get_line(int fd, char **sline) {
+    size_t reserved_size = 1;
+    size_t used_size = 0;
+
+    // TODO: Fix this, on several calls is trashing previous memory
+    *sline = (char *)malloc(1);
+    char *line = *sline;
+    char c;
+    int status;
+    while (true) {
+        status = read(fd, &c, 1);
+
+        if (status == 0 || status == -1)
+            return status;
+
+        if (c == '\n') {
+            if (reserved_size == used_size) {
+                realloc(*sline, reserved_size + 1);
+            } else {
+                realloc(*sline, used_size + 1);
+            }
+
+            line[used_size] = '\0';
+            return 1;
+        }
+
+        if (reserved_size == used_size) {
+            reserved_size *= 2;
+            realloc(*sline, reserved_size);
+        }
+
+        line[used_size++] = c;
+    }
+}
+
+char *load_name_from_status_file(const char *path) {
+    int status_fd = open(path, O_RDONLY);
+    char *line = NULL;
+    while (get_line(status_fd, &line) != 0) {}
+
+    return line;
+}
 
 int main(int argc, char *argv[]) {
     // initializes screen
     // set ups memory and clear screen
     lyrics_t *lyrics = create_lyrics();
     load_lyric_from_file(lyrics, "test.lyric");
+    char *status = load_name_from_status_file(status_path);
+    printf("Status\n%s\n", status);
+
 
     //printf("Lyrics:\n %s", lyrics->lyrics);
+
+    return 0;
 
     initscr();
 
